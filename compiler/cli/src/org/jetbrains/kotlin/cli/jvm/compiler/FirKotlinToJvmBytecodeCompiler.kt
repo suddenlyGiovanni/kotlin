63daf28d4add5f36de3196b90b0bb2fef5157953
Georgy Bronnikov <georgy.bronnikov@jetbrains.com>
2021-12-09 16:37:06 +0300

    Fir2Ir: remove symbolTable as separate component from FirResult
    It's already in components.

--- compiler/cli/src/org/jetbrains/kotlin/cli/jvm/compiler/FirKotlinToJvmBytecodeCompiler.kt
+++ compiler/cli/src/org/jetbrains/kotlin/cli/jvm/compiler/FirKotlinToJvmBytecodeCompiler.kt
@@ -295,7 +295,7 @@ object FirKotlinToJvmBytecodeCompiler {
         session: FirSession,
         diagnosticsReporter: BaseDiagnosticsCollector
     ): GenerationState {
-        val (moduleFragment, symbolTable, components) = fir2IrResult
+        val (moduleFragment, components) = fir2IrResult
         val dummyBindingContext = NoScopeRecordCliBindingTrace().bindingContext
         val codegenFactory = JvmIrCodegenFactory(
             moduleConfiguration,
@@ -325,7 +325,8 @@ object FirKotlinToJvmBytecodeCompiler {
         performanceManager?.notifyIRLoweringStarted()
         generationState.beforeCompile()
         codegenFactory.generateModuleInFrontendIRMode(
-            generationState, moduleFragment, symbolTable, components.irProviders, extensions, FirJvmBackendExtension(session, components)
+            generationState, moduleFragment, components.symbolTable, components.irProviders,
+            extensions, FirJvmBackendExtension(session, components)
         ) {
             performanceManager?.notifyIRLoweringFinished()
             performanceManager?.notifyIRGenerationStarted()
