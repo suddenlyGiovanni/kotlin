3e2beee31f20976620987a685ba74512fcdbfde2
Svyatoslav Scherbina <svyatoslav.scherbina@jetbrains.com>
2022-02-17 10:26:21 +0300

    ObjCExport: enable unitSuspendFunctionObjCExport=proper by default
    For Unit-returning suspend functions ObjCExport now generates
    completion handlers without redundant `KotlinUnit*` result parameter.
    
    So Swift (5.5+) can import these functions as Void-returning async
    functions.
    
    unitSuspendFunctionObjCExport=legacy binary option can be used to
    enable the legacy behaviour. It will be removed in a future release.
    
    ^KT-49928 Fixed

--- kotlin-native/backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/KonanConfig.kt
+++ kotlin-native/backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/KonanConfig.kt
@@ -326,7 +326,7 @@ class KonanConfig(val project: Project, val configuration: CompilerConfiguration
     }
 
     internal val unitSuspendFunctionObjCExport: UnitSuspendFunctionObjCExport
-        get() = configuration.get(BinaryOptions.unitSuspendFunctionObjCExport) ?: UnitSuspendFunctionObjCExport.LEGACY
+        get() = configuration.get(BinaryOptions.unitSuspendFunctionObjCExport) ?: UnitSuspendFunctionObjCExport.DEFAULT
 }
 
 fun CompilerConfiguration.report(priority: CompilerMessageSeverity, message: String)
